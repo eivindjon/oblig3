4cb647eaff4fe626b60e97b53508cf647eb88e71
eivindjon <eivind.b.jonassen@gmail.com>
2021-09-30 20:49:15 +0200

    jaja

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    console.log("CLICKED")
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
 const questionsAndAnswers = [
     {
         question: "",
